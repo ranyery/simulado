c2386253b96fcb93398db1e9bf2e12b4a3bebefe
ranyery <ranyerysc@gmail.com>
2023-08-05 12:09:19 -0300

    feat(client): add home routing

--- apps/client/src/app/modules/home/home.module.ts
+++ apps/client/src/app/modules/home/home.module.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { HomeRoutingModule } from './home-routing.module';
-import { HomePage } from './home.page';
+import { HomePage } from './pages/home.page';
 
 @NgModule({
   declarations: [HomePage],
