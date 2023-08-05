813af6b4390535d31299dfafee6eec79f2e3da71
ranyery <ranyerysc@gmail.com>
2023-08-05 16:50:51 -0300

    feat(home): add question component

--- apps/client/src/app/modules/home/home.module.ts
+++ apps/client/src/app/modules/home/home.module.ts
@@ -1,11 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { RadioButtonModule } from 'primeng/radiobutton';
 
+import { QuestionComponent } from './components/question/question.component';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomePage } from './pages/home.page';
 
 @NgModule({
-  declarations: [HomePage],
-  imports: [CommonModule, HomeRoutingModule],
+  declarations: [HomePage, QuestionComponent],
+  imports: [CommonModule, HomeRoutingModule, ReactiveFormsModule, RadioButtonModule, ButtonModule],
 })
 export class HomeModule {}
