0ddf87d0f11087a64f5658afd044347bde5d1a00
ranyery <ranyerysc@gmail.com>
2023-08-13 22:16:45 -0300

    chore: update scripts

--- apps/server/src/infra/database/prisma.service.ts
+++ apps/server/src/infra/database/prisma.service.ts
@@ -1,9 +1,12 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
+    if (environment.production) return;
+
     await this.$connect();
   }
 }
