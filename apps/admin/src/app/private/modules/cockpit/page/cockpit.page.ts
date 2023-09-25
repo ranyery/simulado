317889a5a91765e44687960446f792abaccdb1aa
ranyery <ranyerysc@gmail.com>
2023-09-25 17:24:46 -0300

    feat(admin): add initial cockpit home

--- apps/admin/src/app/private/modules/cockpit/page/cockpit.page.ts
+++ apps/admin/src/app/private/modules/cockpit/page/cockpit.page.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+
+import { AuthService } from '../../../../shared/services/auth.service';
 
 @Component({
   selector: 'app-cockpit',
   templateUrl: './cockpit.page.html',
   styleUrls: ['./cockpit.page.scss'],
 })
-export class CockpitPage {}
+export class CockpitPage {
+  private readonly _authService = inject(AuthService);
+
+  public readonly items = [
+    { icon: 'pi-home', label: 'Home', route: '' },
+    { icon: 'pi-star', label: 'Dashboard', route: 'dashboard' },
+    { icon: 'pi-user', label: 'Quizzes', route: 'quizzes' },
+    { icon: 'pi-sliders-h', label: 'Exams', route: 'exams' },
+  ];
+
+  public logout(): void {
+    this._authService.logout();
+  }
+}
