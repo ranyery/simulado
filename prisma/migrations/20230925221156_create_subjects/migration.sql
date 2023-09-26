74e7968f3e62840c4bf53be7cbec75fb06540aef
ranyery <ranyerysc@gmail.com>
2023-09-25 22:36:27 -0300

    feat(server): update entities

--- prisma/migrations/20230925221156_create_subjects/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "subjects" (
-    "id" TEXT NOT NULL,
-    "text" TEXT NOT NULL,
-    "description" TEXT NOT NULL,
-
-    CONSTRAINT "subjects_pkey" PRIMARY KEY ("id")
-);
--- prisma/migrations/20230925224948_update_description_subject/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "subjects" ALTER COLUMN "description" DROP NOT NULL;
