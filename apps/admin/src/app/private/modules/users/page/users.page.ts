e209834c6ddd924fd269739402f4a92ad9146b1e
ranyery <ranyerysc@gmail.com>
2023-10-07 18:32:30 -0300

    feat(admin): add mock to users permission page

--- apps/admin/src/app/private/modules/users/page/users.page.ts
+++ apps/admin/src/app/private/modules/users/page/users.page.ts
@@ -1,4 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+import { IEntityPermission } from '@libs/shared/domain';
+import { UserRolesService } from '../../../../shared/services/user-roles.service';
+
+interface IPermissionForm {
+  entity: FormControl<string | null>;
+  read: FormControl<boolean | null>;
+  create: FormControl<boolean | null>;
+  update: FormControl<boolean | null>;
+  delete: FormControl<boolean | null>;
+}
+
+const permissions: IEntityPermission[] = [
+  { entity: 'users', read: true, create: true, update: true, delete: false },
+  { entity: 'subjects', read: true, create: true, update: true, delete: false },
+  { entity: 'topics', read: true, create: false, update: false, delete: false },
+];
 
 @Component({
   selector: 'app-users',
@@ -6,7 +24,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.page.scss'],
 })
 export class UsersPage implements OnInit {
+  private readonly _userRolesService = inject(UserRolesService);
+
+  public entityPermissionForms: FormGroup<IPermissionForm>[] = [];
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    const isAdmin = this._userRolesService.isAdmin();
+    permissions.forEach((permission) => {
+      this.entityPermissionForms.push(
+        new FormGroup({
+          entity: new FormControl<string>(
+            { value: permission.entity, disabled: false },
+            Validators.required
+          ),
+          read: new FormControl<boolean>({ value: permission.read, disabled: !isAdmin }),
+          create: new FormControl<boolean>({ value: permission.create, disabled: !isAdmin }),
+          update: new FormControl<boolean>({ value: permission.update, disabled: !isAdmin }),
+          delete: new FormControl<boolean>({ value: permission.delete, disabled: !isAdmin }),
+        })
+      );
+    });
+  }
+
+  public addPermission(): void {
+    this.entityPermissionForms.push(
+      new FormGroup({
+        entity: new FormControl<string>({ value: '', disabled: false }, Validators.required),
+        read: new FormControl<boolean>({ value: false, disabled: false }),
+        create: new FormControl<boolean>({ value: false, disabled: false }),
+        update: new FormControl<boolean>({ value: false, disabled: false }),
+        delete: new FormControl<boolean>({ value: false, disabled: true }),
+      })
+    );
+  }
+
+  public deletePermission(index: number): void {
+    this.entityPermissionForms.splice(index, 1);
+  }
+
+  public updatePermission(form: FormGroup<IPermissionForm>): void {
+    console.log(form.value);
+  }
+
+  public salvar(): void {
+    const permissions = this.entityPermissionForms.map((form) => form.value);
+    console.log('permissions:', permissions);
+  }
 }
--- apps/admin/src/app/private/modules/users/users.module.ts
+++ apps/admin/src/app/private/modules/users/users.module.ts
@@ -1,11 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { InputSwitchModule } from 'primeng/inputswitch';
 
 import { UsersPage } from './page/users.page';
 import { UsersRoutingModule } from './users-routing.module';
 
 @NgModule({
   declarations: [UsersPage],
-  imports: [CommonModule, UsersRoutingModule],
+  imports: [CommonModule, UsersRoutingModule, ReactiveFormsModule, InputSwitchModule, ButtonModule],
 })
 export class UsersModule {}
