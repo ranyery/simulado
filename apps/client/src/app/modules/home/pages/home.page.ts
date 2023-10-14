d21a7644bd2a5885ed919b50fa3a371762b14a21
ranyery <ranyerysc@gmail.com>
2023-10-14 12:43:44 -0300

    fix(client): adjust image and katex previews

--- apps/client/src/app/modules/home/pages/home.page.ts
+++ apps/client/src/app/modules/home/pages/home.page.ts
@@ -5,11 +5,6 @@ import { ContentService, IContent } from '@simulado/services';
 
 import { QuestionsService } from '../services/questions.service';
 
-interface IAnswer {
-  id: string;
-  title: string;
-}
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
@@ -27,14 +22,7 @@ export class HomePage implements OnInit {
 
   public isLoading: boolean = false;
   public readonly upperLetters: string[] = ['A', 'B', 'C', 'D', 'E'];
-  public answers: IAnswer[] = [
-    { id: 'A', title: '2,40' },
-    { id: 'B', title: '2,35' },
-    { id: 'C', title: '2,45' },
-    { id: 'D', title: '2,50' },
-    { id: 'E', title: '2,55' },
-  ];
-  public topics: string[] = [];
+  public topics: string[] = ['Matemática Financeira', 'Regra de três', 'Equação de 1º grau'];
 
   public form = new FormGroup({
     selectedAnswer: new FormControl(),
@@ -43,13 +31,10 @@ export class HomePage implements OnInit {
   constructor() {}
 
   ngOnInit(): void {
-    this.topics = ['Matemática Financeira', 'Regra de três', 'Equação de 1º grau'];
     this._questionsService.getById('clnmvkg4i002iwmt8mz7lfiuv').subscribe({
       next: (question) => {
         this.question = question;
-        this.contentParts = this._contentService.processHTMLTextWithImageTags(
-          this.question.statement
-        );
+        this.contentParts = this._contentService.processHTMLTextWithImageTags(question.statement);
       },
       error: () => {},
     });
